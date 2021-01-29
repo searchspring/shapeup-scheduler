b99020bd75cd2e2a9c438c1e92d942d4eb265db4
codeallthethingz <howdoicontactwill@gmail.com>
2021-01-29 15:29:09 -0800

    added link to setup

--- app/components/Links.js
+++ app/components/Links.js
@@ -3,6 +3,7 @@ const m = require('mithril')
 module.exports = {
     view: (vnode) => {
         return <div class="text-sm float-right mr-4 mt-3">
+            <a href="#!/setup" class="ml-4 cursor-pointer hover:underline">Setup</a>
             <a href="#!/privacy" class="ml-4 cursor-pointer hover:underline">Privacy</a>
             <a href="#!/terms" class="ml-4 cursor-pointer hover:underline">Terms</a>
             <a target="_blank" href="https://searchspring.com/careers/" class="ml-4 cursor-pointer hover:underline">We hiring!</a>
